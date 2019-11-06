51c91763835f82970a8f3ef15e0cf195adbb3c87
Jalal Kiani <jalalkiani@Jalals-MacBook-Air.local>
2019-11-06 15:58:01 -0500

    data processing is added

---
+++ data_processing.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import numpy as np
+
+
+def get_data(user_sample, test_set=False):
+    '''
+    The user_sample is a DataFrame from train or test where the only one 
+    installation_id is filtered
+    And the test_set parameter is related with the labels processing, that is only requered
+    if test_set=False
+    '''
+    # Constants and parameters declaration
+    last_activity = 0
+    user_activities_count = {'Clip':0, 'Activity': 0, 'Assessment': 0, 'Game':0}
+    accuracy_groups = {0:0, 1:0, 2:0, 3:0}
+    all_assessments = []
+    accumulated_accuracy_group = 0
+    accumulated_accuracy=0
+    accumulated_correct_attempts = 0 
+    accumulated_uncorrect_attempts = 0 
+    accumulated_actions = 0
+    counter = 0
+    durations = []
+    # itarates through each session of one instalation_id
+    for i, session in user_sample.groupby('game_session', sort=False):
+        # i = game_session_id
+        # session is a DataFrame that contain only one game_session
+        
+        # get some sessions information
+        session_type = session['type'].iloc[0]
+        session_title = session['title'].iloc[0]
+        
+        # for each assessment, and only this kind off session, the features below are processed
+        # and a register are generated
+        if (session_type == 'Assessment') & (test_set or len(session)>1):
+            # search for event_code 4100, that represents the assessments trial
+            all_attempts = session.query(f'event_code == {win_code[session_title]}')
+            # then, check the numbers of wins and the number of losses
+            true_attempts = all_attempts['event_data'].str.contains('true').sum()
+            false_attempts = all_attempts['event_data'].str.contains('false').sum()
+            # copy a dict to use as feature template, it's initialized with some itens: 
+            # {'Clip':0, 'Activity': 0, 'Assessment': 0, 'Game':0}
+            features = user_activities_count.copy()
+            # add title as feature, remembering that title represents the name of the game
+            features['session_title'] = session['title'].iloc[0] 
+            # the 4 lines below add the feature of the history of the trials of this player
+            # this is based on the all time attempts so far, at the moment of this assessment
+            features['accumulated_correct_attempts'] = accumulated_correct_attempts
+            features['accumulated_uncorrect_attempts'] = accumulated_uncorrect_attempts
+            accumulated_correct_attempts += true_attempts 
+            accumulated_uncorrect_attempts += false_attempts
+            # the time spent in the app so far
+            if durations == []:
+                features['duration_mean'] = 0
+            else:
+                features['duration_mean'] = np.mean(durations)
+            durations.append((session.iloc[-1, 2] - session.iloc[0, 2] ).seconds)
+            # the accurace is the all time wins divided by the all time attempts
+            features['accumulated_accuracy'] = accumulated_accuracy/counter if counter > 0 else 0
+            accuracy = true_attempts/(true_attempts+false_attempts) if (true_attempts+false_attempts) != 0 else 0
+            accumulated_accuracy += accuracy
+            # a feature of the current accuracy categorized
+            # it is a counter of how many times this player was in each accuracy group
+            if accuracy == 0:
+                features['accuracy_group'] = 0
+            elif accuracy == 1:
+                features['accuracy_group'] = 3
+            elif accuracy == 0.5:
+                features['accuracy_group'] = 2
+            else:
+                features['accuracy_group'] = 1
+            features.update(accuracy_groups)
+            accuracy_groups[features['accuracy_group']] += 1
+            # mean of the all accuracy groups of this player
+            features['accumulated_accuracy_group'] = accumulated_accuracy_group/counter if counter > 0 else 0
+            accumulated_accuracy_group += features['accuracy_group']
+            # how many actions the player has done so far, it is initialized as 0 and updated some lines below
+            features['accumulated_actions'] = accumulated_actions
+            
+            # there are some conditions to allow this features to be inserted in the datasets
+            # if it's a test set, all sessions belong to the final dataset
+            # it it's a train, needs to be passed throught this clausule: session.query(f'event_code == {win_code[session_title]}')
+            # that means, must exist an event_code 4100 or 4110
+            if test_set:
+                all_assessments.append(features)
+            elif true_attempts+false_attempts > 0:
+                all_assessments.append(features)
+                
+            counter += 1
+
+        # counts how many actions the player has done so far, used in the feature of the same name
+        accumulated_actions += len(session)
+        if last_activity != session_type:
+            user_activities_count[session_type] += 1
+            last_activitiy = session_type
+    # if it't the test_set, only the last assessment must be predicted, the previous are scraped
+    if test_set:
+        return all_assessments[-1]
+    # in the train_set, all assessments goes to the dataset
+    return all_assessments
